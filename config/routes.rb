24522c0efd00640ee244b22800c28c7aa5516cc4
masahisazumi <m.hisazumi@gmail.com>
2023-01-06 10:53:44 +0900

    pages home

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users,
     path: '',
     path_names: {sign_up: 'register', sign_in: 'login', edit: 'profile', sign_out: 'logout'},
